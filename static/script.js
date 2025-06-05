dba8b41002c4838090d986c3c58d8afd5d186c66
abinashasp <108266770+abinashasp@users.noreply.github.com>
2025-06-05 07:20:40 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,44 @@
 
+
+async function fetchEvents() {
+    try {
+        const res = await fetch('/events');
+        const events = await res.json();
+
+        const ul = document.getElementById('eventList');
+        ul.innerHTML = ""; 
+
+        events.forEach(event => {
+            const li = document.createElement('li');
+            const date = new Date(event.timestamp).toUTCString();
+            let message = "";
+
+            switch (event.action_type) {
+                case "push":
+                    message = `${event.author} pushed to ${event.to_branch} on ${date}`;
+                    break;
+                case "pull_request":
+                    message = `${event.author} submitted a pull request from ${event.from_branch} to ${event.to_branch} on ${date}`;
+                    break;
+                case "merge":
+                    message = `${event.author} merged branch ${event.from_branch} to ${event.to_branch} on ${date}`;
+                    break;
+                default:
+                    message = "Unknown event";
+            }
+
+            li.textContent = message;
+            ul.appendChild(li);
+        });
+
+    } catch (error) {
+        console.error("Error fetching events:", error);
+    }
+}
+
+
+fetchEvents();
+
+
+setInterval(fetchEvents, 15000);
+
